91e218b7af1db60218f623824e84146a862262f1
dennuguyen <dennuguyen@gmail.com>
2020-08-31 14:52:10 +1000

    Add empty member to shape struct, getters and equality()

--- include/shape.hpp
+++ include/shape.hpp
@@ -3,6 +3,7 @@
 
 #include <vector>
 
+#include "ray.hpp"
 #include "tuple.hpp"
 
 namespace shape
@@ -11,21 +12,30 @@ class Shape
 {
 public:
     Shape(void);
+    int get_id();
 
 protected:
+    bool empty;
     int id;
 };
 
 class Sphere : public Shape
 {
-public:
+    public:
     Sphere(void);
     Sphere(tuple::Tuple origin_, double radius_);
+    ~Sphere();
 
-private:
+    tuple::Tuple get_origin();
+    double get_radius();
+
+    private:
     tuple::Tuple origin;
     double radius;
 };
+
+bool is_same_shape_object(Shape a, Shape b);
+inline bool operator==(Shape a, Shape b) { return is_same_shape_object(a, b); };
 
 } // namespace shape
 
--- src/shape.cpp
+++ src/shape.cpp
@@ -1,23 +1,35 @@
 #include "shape.hpp"
 
+#include <cmath>
+
 namespace shape
 {
 int global_id = 0;
 
-Shape::Shape(void) : id(global_id++)
-{
-}
+Shape::Shape(void) : empty(true), id(global_id++) {}
+
+int Shape::get_id() { return id; }
+
+bool is_same_shape_object(Shape a, Shape b) { return a.get_id() == b.get_id(); }
 
 Sphere::Sphere(void)
     : origin(tuple::make_point(0.0, 0.0, 0.0))
     , radius(1.0)
 {
+    empty = false;
 }
 
-Sphere::Sphere(tuple::Tuple origin_, double radius_) 
+Sphere::Sphere(tuple::Tuple origin_, double radius_)
     : origin(origin_)
     , radius(radius_)
 {
+    empty = false;
 }
+
+Sphere::~Sphere() {}
+
+tuple::Tuple Sphere::get_origin() { return origin; }
+
+double Sphere::get_radius() { return radius; }
 
 } // namespace shape
