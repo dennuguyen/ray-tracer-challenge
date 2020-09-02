bfbd1c2faa9e34cd33f4f0d79b91bb84c413f3a5
dennuguyen <dennuguyen@gmail.com>
2020-09-02 17:07:29 +1000

    Overload colour operators

--- include/colour.hpp
+++ include/colour.hpp
@@ -17,20 +17,30 @@ struct Colour {
     double b;
 };
 
+// Equality Operator
 bool is_same_colour(Colour a, Colour b);
-inline bool operator==(Colour a, Colour b) { return is_same_colour(a, b); };
 
+// Colour Operators
 Colour add(Colour a, Colour b);
 Colour minus(Colour a, Colour b);
 Colour scalar_multiply(Colour a, double scalar);
 Colour colour_multiply(Colour a, Colour b);
 
+// Overloaded Operators
+inline bool operator==(Colour a, Colour b) { return is_same_colour(a, b); };
+inline Colour operator+(Colour a, Colour b) { return add(a, b); };
+inline Colour operator-(Colour a, Colour b) { return minus(a, b); };
+inline Colour operator^(Colour a, double scalar) { return scalar_multiply(a, scalar); };
+inline Colour operator*(Colour a, Colour b) { return colour_multiply(a, b); };
+
+// Colour Palette
 const Colour NONE(NOFILL, NOFILL, NOFILL);
 const Colour BLACK(0.0, 0.0, 0.0);
 const Colour WHITE(1.0, 1.0, 1.0);
 const Colour RED(1.0, 0.0, 0.0);
 const Colour GREEN(0.0, 1.0, 0.0);
 const Colour BLUE(0.0, 0.0, 1.0);
+
 }  // namespace colour
 
 #endif  // COLOUR_HPP_
