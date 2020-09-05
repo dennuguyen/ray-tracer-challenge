c88b69d6d06723b135db84687b3be74cf014239e
dennuguyen <dennuguyen@gmail.com>
2020-09-05 16:46:52 +1000

    Construct World with obj and light and add getters

--- include/world.hpp
+++ include/world.hpp
@@ -10,6 +10,10 @@ class World
 {
 public:
     World(void);
+    World(std::vector<shape::Shape> obj_, std::vector<shades::PointLight> light_);
+
+    const std::vector<shape::Shape> get_objects(void);
+    const std::vector<shades::PointLight> get_lights(void);
 
 private:
     std::vector<shape::Shape> obj;
--- src/world.cpp
+++ src/world.cpp
@@ -4,5 +4,12 @@ namespace scene
 {
 World::World(void) : obj({}), light({}) {}
 
+World::World(std::vector<shape::Shape> obj_, std::vector<shades::PointLight> light_)
+    : obj(obj_)
+    , light(light_)
+{}
+
+const std::vector<shape::Shape> World::get_objects(void) { return obj; }
+const std::vector<shades::PointLight> World::get_lights(void) { return light; }
 
 } // namespace scene
